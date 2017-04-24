b4f593cffce907e91a27df2d8ce45885230565ec
Hossam <jkasdhkjadas>
2017-04-24 21:55:08 +0200

    final Commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,34 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/:time',function(req,res) {
+
+  var unixToNatural = function(unix) {
+    var date = new Date(unix * 1000);
+    var monthNames = ["January", "February", "March",       "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    var naturalDate = monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear()
+    return naturalDate;
+  }
+  var input = req.params.time;
+  //
+  if (!isNaN(input)) {
+    var unix = input;
+    var naturalDate = unixToNatural(unix);
+    var data = {unix:input, natural:naturalDate};
+    res.send(data);
+  } else {
+    console.log(input);
+    var date = new Date(input);
+    if(!isNaN(date)) {
+      var data =  {unix:date/1000, natural:input};
+      res.send(data);
+    } else {
+      var data = {unix:null,natural:null};
+      res.send(data);
+    }
+  }
+});
+
 module.exports = router;
